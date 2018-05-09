ddc3795dbfb3740338950a5007eb77757740f176
u4bi <admin@naver.id>
2018-05-10 02:13:04 +0900

    Added sign and verify for jwt.

--- server/src/c_jwt/core.clj
+++ server/src/c_jwt/core.clj
@@ -4,16 +4,37 @@
             [compojure.handler :as h]
             [compojure.route :as route]
             [ring.middleware.json :as m-json]
-            [ring.middleware.cors :refer [wrap-cors]]))
+            [ring.middleware.cors :refer [wrap-cors]]
+            [clj-jwt.core  :refer :all]
+            [clj-time.core :refer [now plus days]]))
+
+(def hs-prv-key "secretKEYu4bi-test-1234@#")
+
+(defn sign-key [name]
+  (-> {:name "u4bi"
+       :level 17
+       :stats {:healing 1700
+               :hungry 530
+               :thirsty 950
+               :stuffed 900}
+       :bio nil
+       :exp (plus (now) (days 1))
+       :iat (now)} jwt (sign :HS256 hs-prv-key) to-str))
+
+(defn verify-token [token]
+  (-> token str->jwt (verify hs-prv-key)))
+
+(defn decode-token [token]
+  (-> token str->jwt :claims))
 
 (defroutes handler
            (POST "/sign" [userid, accesskey]
                  (response {
-                            :userid userid
-                            :accesskey accesskey}))
-           (POST "/verify" [token]
-                 (response {
-                            :token token}))
+                            :token (sign-key name)}))
+           (POST "/verify/:token" [token]
+                 (response (if-let [is-verified (verify-token token)]
+                             {:verify is-verified :data (decode-token token)}
+                             {:verify false})))
            (route/not-found (response
                              {:message "not found"})))
   
